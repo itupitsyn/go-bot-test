ce4b0852d9cf4604f634539a91635fbf64dd6824
itupitsyn <itupitsyn@ptsecurity.com>
2024-02-01 10:16:44 +0300

    fix(stats): month fix

--- model/Raffle.go
+++ model/Raffle.go
@@ -45,7 +45,7 @@ type Stats struct {
 func GetStats(chatId int64) *[]Stats {
 	var result []Stats
 	now := time.Now().In(time.UTC)
-	beginningOfYear := time.Date(now.Year(), now.Month(), 1, 0, 0, 0, 0, time.UTC)
+	beginningOfYear := time.Date(now.Year(), 1, 1, 0, 0, 0, 0, time.UTC)
 	subQuery := database.Database.Model(&Raffle{}).Select("count (*) as Count, winner_id").Where("winner_id is not null and chat_id = ? and date >= ?", chatId, beginningOfYear).Group("winner_id")
 	database.Database.Model(&User{}).Select("users.name as Name, users.alternative_name as Alternativename, kk.Count").Joins("inner join (?) as kk on users.id = kk.winner_id", subQuery).Order("Count desc").Find(&result)
 	return &result
